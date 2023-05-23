2bf5f8edf21c41f3b4e18a231ec9413f6c6b4dac
Victor Viana <victorvpaiva@alu.ufc.br>
2023-05-23 19:36:58 -0300

    Criacao do projeto no xcode

---
+++ HortCult/HortCult/HortCultApp.swift
@@ -0,0 +1,20 @@
+//
+//  HortCultApp.swift
+//  HortCult
+//
+//  Created by userext on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct HortCultApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
