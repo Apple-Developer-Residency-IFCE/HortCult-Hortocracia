6c65d94d1cc389f98ea1ad0f3d6b1eb576f20d24
João Vitor <jvholanda18@gmail.com>
2023-05-25 19:59:10 -0300

    Cria botões das telas de onboarding,cria textos da tela de onboarding, cria a logo pertecente a onboarding, cria a funcao que seleciona a imagens para serem mostradas na tela de onboarding, cria os botoes de pular para tela inicial, cria telas de teste de funcionamento das funcoes, cria a tela de splash, adiciona imagens, e cores usadas na demanda,

---
+++ HortCult/HortCult/Views/Onboarding/Onboarding.swift
@@ -0,0 +1,20 @@
+//
+//  Onboarding.swift
+//  HortCult
+//
+//  Created by userext on 25/05/23.
+//
+
+import SwiftUI
+
+struct Onboarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Onboarding_Previews: PreviewProvider {
+    static var previews: some View {
+        Onboarding()
+    }
+}
---
+++ HortCult/HortCult/Views/Onboarding/Splash.swift
@@ -0,0 +1,53 @@
+//
+//  Splash.swift
+//  HortCult
+//
+//  Created by userext on 25/05/23.
+//
+
+import SwiftUI
+
+struct Splash: View {
+    
+    
+    
+    @State var isActive: Bool = false
+    @State var size = 0.8
+    @State var opacity = 0.5
+    var body: some View {
+        if isActive {
+            TestesTelasHortcult()
+        } else {
+            VStack {
+                VStack {
+                    Image("Logo_Clara")
+                        .resizable()
+                        .frame(width: 257, height: 47.26)
+                }
+                .scaleEffect(size)
+                .opacity(opacity)
+                .onAppear {
+                    withAnimation(.easeIn(duration: 1.2)) {
+                        self.size = 0.9
+                        self.opacity = 1.00
+                    }
+                }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    withAnimation {
+                        self.isActive = true
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+struct Splash_Previews: PreviewProvider {
+    static var previews: some View {
+        Splash()
+    }
+}
+
