e57463fe1901323798aef6e937e19427086d81b3
João Vitor Alves Holanda <vitor@10-45-51-226.0200.ifce.br.iosda.org>
2023-06-06 20:30:13 -0300

    Cria a tela de home, e adiciona lista de plantas, e lista lembretes. Pendente ainda, o espaçamento entre os elementos e ajustes finos.

---
+++ HortCult/HortCult/Views/AddVegetable/AddVegetable.swift
@@ -0,0 +1,20 @@
+//
+//  AddVegetable.swift
+//  HortCult
+//
+//  Created by João Vitor Alves Holanda on 06/06/23.
+//
+
+import SwiftUI
+
+struct AddVegetable: View {
+    var body: some View {
+        Text("Adicionar Vegetal")
+    }
+}
+
+struct AddVegetable_Previews: PreviewProvider {
+    static var previews: some View {
+        AddVegetable()
+    }
+}
