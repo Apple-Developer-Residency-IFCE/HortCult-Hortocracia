2ae4ebf7712b2c348dc96847ba20077ae2383675
jvholanda2 <jvholanda18@gmail.com>
2023-06-14 18:57:54 -0300

    Cria os elementos de nome e descricao da tela de adicao de vegetais.

--- HortCult/HortCult/Views/AddVegetable/NameDescription.swift
+++ HortCult/HortCult/Views/AddVegetable/NameDescription.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct NameDescription: View {
     
-    @State private var text: String = ""
+    @State private var nameVegetable: String = ""
+    @State private var descriptionVegetable: String = ""
     
     var body: some View {
         VStack(alignment: .leading) {
@@ -17,24 +18,36 @@ struct NameDescription: View {
                 .font(.title)
                 .font(.system(size: 28))
                 .fontWeight(.bold)
-                .padding()
+                .padding(.bottom, 20)
                 .foregroundColor(Color("H1Color"))
             Text("Nome")
                 .foregroundColor(Color("Cinza"))
                 .fontWeight(.light)
                 .font(.system(size: 12))
-            TextField("Insira um nome", text: $text)
-                //.textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
+            TextField("Insira um nome", text: $nameVegetable)
+                .padding(.leading, 20)
+                .frame(height: 38)
                 .overlay(
                     RoundedRectangle(cornerRadius: 50)
-                        .stroke(Color("Cinza"), lineWidth: 1)
-                )               // .border(.blue)
-
-
+                        .stroke(Color("CinzaClaro"), lineWidth: 1)
+                )
+            Spacer().frame(height: 28)
+            Text("Descrição")
+                .foregroundColor(Color("Cinza"))
+                .fontWeight(.light)
+                .font(.system(size: 12))
+            TextField("Insira uma descrição", text: $descriptionVegetable)
+                .padding(.bottom, 160)
+                .padding(.leading, 15)
+                .frame(height: 200)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color("CinzaClaro"), lineWidth: 1)
+                )
+            
+            
         }
         .padding()
-        .border(.red)
     }
 }
 
