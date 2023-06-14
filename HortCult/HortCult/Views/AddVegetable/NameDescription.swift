fd48b6c401b4445c74ec416063624e34140a4eb3
jvholanda2 <jvholanda18@gmail.com>
2023-06-13 21:34:16 -0300

    cria os elementos de nome e descricao da adicao de vegetais

---
+++ HortCult/HortCult/Views/AddVegetable/NameDescription.swift
@@ -0,0 +1,20 @@
+//
+//  NameDescription.swift
+//  HortCult
+//
+//  Created by João Vitor Alves Holanda on 13/06/23.
+//
+
+import SwiftUI
+
+struct NameDescription: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NameDescription_Previews: PreviewProvider {
+    static var previews: some View {
+        NameDescription()
+    }
+}
