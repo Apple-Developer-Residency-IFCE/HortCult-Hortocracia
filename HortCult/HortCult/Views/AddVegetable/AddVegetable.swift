7b298ac909fb288bb73a52849a9d880fc83da298
jvholanda2 <jvholanda18@gmail.com>
2023-06-14 21:36:45 -0300

    Inicia a criacao da tela de adicao e edicao de vegetais

--- HortCult/HortCult/Views/AddVegetable/AddVegetable.swift
+++ HortCult/HortCult/Views/AddVegetable/AddVegetable.swift
@@ -10,7 +10,18 @@ import SwiftUI
 struct AddVegetable: View {
     
     var body: some View {
-        Text("oi")
+        NavigationView {
+            ScrollView(.vertical){
+                AddEditTitle(addEdit: true)
+                NameDescription()
+            }.toolbar(){
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Image("Topbar")
+                }
+            }
+        }
+
+
     }
 }
 
@@ -19,3 +30,6 @@ struct AddVegetable_Previews: PreviewProvider {
         AddVegetable()
     }
 }
+
+
+
