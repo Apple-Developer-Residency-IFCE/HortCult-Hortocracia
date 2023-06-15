cc9a5bfbc8cfcfb10a0da3528ca1d6df8836fd90
PedroHenriqueSantosRodrigues <pedrohenrique10001@gmail.com>
2023-06-14 21:26:04 -0300

    ïmplementacao da funcoes de coredata

---
+++ HortCult/HortCult/Model/Plant+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Plant+CoreDataClass.swift
+//  HortCult
+//
+//  Created by Pedro Henrique Santos Rodrigues on 14/06/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Plant)
+public class Plant: NSManagedObject {
+
+}
