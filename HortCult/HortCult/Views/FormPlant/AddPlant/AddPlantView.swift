664d265d076f8b2124893b5af0e36597c85ada51
João Guilherme <joaogacanuto@gmail.com>
2023-07-06 21:52:01 -0300

    Implementando novo addplant com viewmodel

--- HortCult/HortCult/Views/FormPlant/AddPlant/AddPlantView.swift
+++ HortCult/HortCult/Views/FormPlant/AddPlant/AddPlantView.swift
@@ -8,23 +8,102 @@
 import SwiftUI
 
 struct AddPlantView: View {
-    @EnvironmentObject var viewModel: AddPlantViewModel
+    @EnvironmentObject var addViewModel: AddPlantViewModel
+    @AppStorage ("selectedTheme")private var selectedTheme: Choice?
+    
+    var isFieldsFilled: Bool{
+        return addViewModel.plantName.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty && addViewModel.plantInformation.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty && addViewModel.plantCategory != "Selecionar..." && addViewModel.plantFrequency != "Selecionar..."
+        }
+    
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    
     var load: () -> Void
     
+    var Title: some View {
+        HStack{
+            Text("Adicionar Vegetal")
+                .font(.title)
+                .font(Font.custom("Satoshi-Bold", size: 28))
+                .fontWeight(.bold)
+                .foregroundColor(Color("VerdeEscuro"))
+                .padding(.top, 20)
+        }
+    }
+    
+    var NavBar : some View {
+        ZStack {
+            HeaderLogo()
+                .frame(minWidth: 400, minHeight: 200)
+                .padding(.top,-40)
+            HStack {
+                Button(action:{
+                    self.presentationMode.wrappedValue.dismiss()
+                } ) {
+                    Image(selectedTheme == .Escuro ? "Arrow-Left-Light" : "Arrow-Left-Green")
+                }
+                .padding(.leading, 18)
+            }
+            .padding(.leading,-190)
+        }
+    }
+
     var body: some View {
-        VStack {
-            TextField("Enter name", text: $viewModel.plantName)
-            TextField("Enter information", text: $viewModel.plantInformation)
-            TextField("Enter category", text: $viewModel.plantCategory)
-            TextField("Enter frequency", text: $viewModel.plantFrequency)
-            DatePicker("Select next date", selection: $viewModel.plantNextDate, displayedComponents: .date)
-            Button(action: {
-                viewModel.addPlant()
-                load()
-            }) {
-                Text("Add Plant")
+
+        NavigationView {
+            ZStack{
+                ScrollView(.vertical){
+
+                    ZStack{
+                        VStack(alignment: .leading, spacing: 10){
+                            Title
+                            NameInput(namePlant: $addViewModel.plantName)
+                            DescriptionInput(descriptionVegetable: $addViewModel.plantInformation)
+                            CategoryPicker(selectedOption: $addViewModel.plantCategory)
+                            FrequencyPicker(selectedOption: $addViewModel.plantFrequency)
+                            ImagePicker(selectedPhotosData: $addViewModel.plantImage)
+                            Spacer(minLength: 80)
+                        }
+                        .padding(.horizontal, 40)
+                    }
+                }
+                Spacer()
+
+
+
+                VStack{
+                    Spacer()
+
+                    Button(action: {
+                        addViewModel.addPlant()
+                        load()
+                        self.presentationMode.wrappedValue.dismiss()
+                    }) {
+                        HStack {
+                            Text("Adicionar Novo Vegetal")
+                                .foregroundColor(Color("Branco"))
+                                .font(.system(size: 16))
+                                .bold()
+                        }
+                        .foregroundColor(Color("CinzaClaro"))
+
+                        .frame(width: 350, height: 42)
+                        .background(Color(addViewModel.isAddButtonAble ? "VerdeEscuro" : "CinzaClaro"))
+                        .cornerRadius(40)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 40)
+                                .stroke(Color(addViewModel.isAddButtonAble  ? "VerdeEscuro" : "CinzaClaro"), lineWidth: 2)
+                        )
+                    }
+                    .disabled(!addViewModel.isAddButtonAble)
+                    .frame(alignment: .bottom)
+                    .padding(.bottom, 60)
+                }
             }
         }
-        .padding()
+        .edgesIgnoringSafeArea(.all)
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: NavBar)
+
     }
 }
--- HortCult/HortCult/Views/FormPlant/AddPlant/AddPlantViewModel.swift
+++ HortCult/HortCult/Views/FormPlant/AddPlant/AddPlantViewModel.swift
@@ -8,34 +8,56 @@
 import Foundation
 
 class AddPlantViewModel: ObservableObject {
-    @Published var plantName: String = ""
-    @Published var plantInformation: String = ""
-    @Published var plantCategory: String = ""
-    @Published var plantFrequency: String = ""
+    @Published var plantName: String = ""{
+        didSet {
+            validatesFields()
+        }
+    }
+    
+    @Published var plantInformation: String = "" {
+        didSet {
+            validatesFields()
+        }
+    }
+    
+    @Published var plantCategory: String = ""{
+        didSet {
+            validatesFields()
+        }
+    }
+    
+    @Published var plantFrequency: String = ""{
+        didSet {
+            validatesFields()
+        }
+    }
+    
     @Published var plantNextDate: Date = Date()
     @Published var plantImage:[Data] = []
-    @Published var addButtonIsDisabled: Bool = false
+    @Published var selectedPhotosData:[Data] = []
+    
+    @Published var isAddButtonAble: Bool = false
+    
     let service: CoredataServices
     
     init(service: CoredataServices){
         self.service = service
     }
     
-    // quando eu tenho uma função assim:
-    func validatesFields(name:String) -> Bool{
-        if(name.isEmpty) {return false}
-        else{ return true }
+    func validatesFields() {
+        isAddButtonAble = !plantName.isEmpty && !plantCategory.isEmpty && !plantFrequency.isEmpty && !plantInformation.isEmpty
     }
     
     func addPlant(){
         let _ = service.createPlant(name: plantName, information: plantInformation, category: plantCategory, frequency: plantFrequency, image: plantImage, nextDate: plantNextDate)
+        CoreDataStack.shared.saveContext()
         plantName = ""
         plantInformation = ""
         plantCategory = ""
         plantFrequency = ""
         plantImage = []
         plantNextDate = Date()
-        CoreDataStack.shared.saveContext()
+        
     }
     
     private func resetFields() {
