2ee6d4c3f28edbf293fdc69503357608456b8232
João Guilherme <joaogacanuto@gmail.com>
2023-07-06 21:52:40 -0300

    implementando editplant

--- HortCult/HortCult/Views/FormPlant/EditPlant/EditPlantView.swift
+++ HortCult/HortCult/Views/FormPlant/EditPlant/EditPlantView.swift
@@ -8,13 +8,102 @@
 import SwiftUI
 
 struct EditPlantView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    @EnvironmentObject var editViewModel: EditPlantViewModel
+    @AppStorage ("selectedTheme")private var selectedTheme: Choice?
+    
+    var isFieldsFilled: Bool{
+        return editViewModel.plantName.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty && editViewModel.plantInformation.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty && editViewModel.plantCategory != "Selecionar..." && editViewModel.plantFrequency != "Selecionar..."
+        }
+    
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    
+    
+    var load: () -> Void
+    
+    var Title: some View {
+        HStack{
+            Text("Editar Vegetal")
+                .font(.title)
+                .font(Font.custom("Satoshi-Bold", size: 28))
+                .fontWeight(.bold)
+                .foregroundColor(Color("VerdeEscuro"))
+                .padding(.top, 20)
+        }
     }
-}
+    
+    var NavBar : some View {
+        ZStack {
+            HeaderLogo()
+                .frame(minWidth: 400, minHeight: 200)
+                .padding(.top,-40)
+            HStack {
+                Button(action:{
+                    self.presentationMode.wrappedValue.dismiss()
+                } ) {
+                    Image(selectedTheme == .Escuro ? "Arrow-Left-Light" : "Arrow-Left-Green")
+                }
+                .padding(.leading, 18)
+            }
+            .padding(.leading,-190)
+        }
+    }
 
-struct EditPlantView_Previews: PreviewProvider {
-    static var previews: some View {
-        EditPlantView()
+    var body: some View {
+
+        NavigationView {
+            ZStack{
+                ScrollView(.vertical){
+
+                    ZStack{
+                        VStack(alignment: .leading, spacing: 10){
+                            Title
+                            NameInput(namePlant: $editViewModel.plantName)
+                            DescriptionInput(descriptionVegetable: $editViewModel.plantInformation)
+                            CategoryPicker(selectedOption: $editViewModel.plantCategory)
+                            FrequencyPicker(selectedOption: $editViewModel.plantFrequency)
+                            ImagePicker(selectedPhotosData: $editViewModel.plantImage)
+                            Spacer(minLength: 80)
+                        }
+                        .padding(.horizontal, 40)
+                    }
+                }
+                Spacer()
+
+
+
+                VStack{
+                    Spacer()
+
+                    Button(action: {
+                        self.presentationMode.wrappedValue.dismiss()
+                        editViewModel.updatePlant()
+                        load()
+                    }) {
+                        HStack {
+                            Text("Adicionar Novo Vegetal")
+                                .foregroundColor(Color("Branco"))
+                                .font(.system(size: 16))
+                                .bold()
+                        }
+                        .foregroundColor(Color("CinzaClaro"))
+
+                        .frame(width: 350, height: 42)
+                        .background(Color(isFieldsFilled ? "VerdeEscuro" : "CinzaClaro"))
+                        .cornerRadius(40)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 40)
+                                .stroke(Color(isFieldsFilled  ? "VerdeEscuro" : "CinzaClaro"), lineWidth: 2)
+                        )
+                    }
+                    .disabled(isFieldsFilled )
+                    .frame(alignment: .bottom)
+                    .padding(.bottom, 60)
+                }
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: NavBar)
+
     }
 }
--- HortCult/HortCult/Views/FormPlant/EditPlant/EditPlantViewModel.swift
+++ HortCult/HortCult/Views/FormPlant/EditPlant/EditPlantViewModel.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+class EditPlantViewModel: ObservableObject {
+    @Published var planta:Plant
+    @Published var plantName: String
+    @Published var plantInformation: String
+    @Published var plantCategory: String
+    @Published var plantFrequency: String
+    @Published var plantNextDate: Date
+    @Published var plantImage:[Data]
+    @Published var editButtonIsDisabled: Bool = false
+    @Published var selectedPhotosData:[Data] = []
+    
+    let service: CoredataServices
+    
+    init(service: CoredataServices, planta: Plant){
+        self.planta = planta
+        self.service = service
+        plantName = planta.name ?? ""
+        plantInformation = planta.information ?? ""
+        plantCategory = planta.category ?? ""
+        plantFrequency = planta.frequency ?? ""
+        plantNextDate = planta.nextDate ?? Date()
+        plantImage = planta.image ?? []
+    }
+    
+    func updatePlant(){
+        let _ = service.update(plant: planta,
+                               name: plantName,
+                               information: plantInformation,
+                               category: plantCategory,
+                               frequency: plantFrequency,
+                               image: plantImage,
+                               nextDate: plantNextDate)
+        CoreDataStack.shared.saveContext()
+    }
+}
