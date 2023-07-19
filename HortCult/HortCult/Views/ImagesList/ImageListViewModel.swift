3371cfd3f2a1f1604c2dff3064cfa180557eba32
João Guilherme <joaogacanuto@gmail.com>
2023-07-19 19:38:23 -0300

    implementando logica para a imageList

--- HortCult/HortCult/Views/ImagesList/ImageListViewModel.swift
+++ HortCult/HortCult/Views/ImagesList/ImageListViewModel.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+class ImageListViewModel: ObservableObject {
+    @Published var planta:PlantModel
+    @Published var data:[Data]
+    let service:CoredataServices
+    
+    init(service:CoredataServices, planta:PlantModel){
+        self.service = service
+        self.planta = planta
+        data = planta.image
+    }
+    
+    func getImage(){
+        data = service.fetchPlantModel(by: planta.id).image
+    }
+}
