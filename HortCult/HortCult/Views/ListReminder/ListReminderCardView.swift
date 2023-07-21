23fc8291be6ade912f5a42241f5d34ff0a6d6c26
João Guilherme <joaogacanuto@gmail.com>
2023-07-21 19:06:38 -0300

    fix: colocando titulo de lembretes

--- HortCult/HortCult/Views/ListReminder/ListReminderCardView.swift
+++ HortCult/HortCult/Views/ListReminder/ListReminderCardView.swift
@@ -13,15 +13,23 @@ struct ListReminderCardView: View, ReminderCardDelegate {
     @State var plantasFiltradas:[PlantModel] = []
     
     var body: some View {
-        ScrollView(.vertical){
-            LazyVStack(spacing: 12){
-                ForEach(listReminderViewModel.plantas, id: \.self){ planta in
-                    ReminderCardView(delegate: self)
-                        .environmentObject(ReminderCardViewModel(service: CoredataServices(), planta: planta))
-                    
+        VStack(alignment: .leading) {
+            Text("Lembretes")
+                .font(Font.custom("Satoshi-Bold", size: 28))
+                .fontWeight(.bold)
+                .padding()
+                .foregroundColor(Color("VerdeEscuro"))
+                .padding(.leading, 5)
+            ScrollView(.vertical){
+                LazyVStack(spacing: 12){
+                    ForEach(listReminderViewModel.plantas, id: \.self){ planta in
+                        ReminderCardView(delegate: self)
+                            .environmentObject(ReminderCardViewModel(service: CoredataServices(), planta: planta))
+                        
+                    }
                 }
+                .onAppear(perform: {plantasFiltradas = listReminderViewModel.getPlants()})
             }
-            .onAppear(perform: {plantasFiltradas = listReminderViewModel.getPlants()})
         }
     }
     
